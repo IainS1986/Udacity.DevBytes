e5b994694c933b06eb26002737f84bc07e75c3eb
Iain <i_stanford@hotmail.com>
2020-01-05 15:51:09 +0000

    Lesson 14
    Repository setup for Videos to bridge Network, App, Database together

--- app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
+++ app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -16,3 +16,45 @@
  */
 
 package com.example.android.devbyteviewer.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import com.example.android.devbyteviewer.database.VideoDatabase
+import com.example.android.devbyteviewer.database.asDomainModel
+import com.example.android.devbyteviewer.domain.Video
+import com.example.android.devbyteviewer.network.Network
+import com.example.android.devbyteviewer.network.asDatabaseModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+
+class VideosRepository(private val database: VideoDatabase) {
+
+    /**
+     * A playlist of videos that can be shown in app
+     *
+     * This returns the videos in the database, converting it to the App/Domain model required
+     */
+    val videos: LiveData<List<Video>> = Transformations.map(database.videoDao.getVideos()) {
+        it.asDomainModel()
+    }
+
+    /**
+     * Refresh the videos in the offline cache
+     *
+     * Will run on IO thread and will trigger a network call to fetch new videos
+     */
+    suspend fun refreshVideos() {
+
+        // Run on the IO dispatcher thread as we are reading/writing to disk (Database insert)
+        withContext(Dispatchers.IO){
+
+            // Request
+            val playlist = Network.devbytes.getPlaylist().await()
+
+            // Store Note - * is the "spread" operator and allows arrays to act as varargs (params). Its NOT pointer magic.
+            database.videoDao.insertAll(*playlist.asDatabaseModel())
+
+        }
+    }
+}
